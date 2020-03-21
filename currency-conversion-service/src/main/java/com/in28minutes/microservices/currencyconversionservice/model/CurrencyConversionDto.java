8625444464ae91ea0a8ff5eee6f7f60f7cc43dc5
Yaser <yaser.ghaderipoor@gmail.com>
2020-03-21 16:57:49 +0430

    Microservices with spring cloud Step 20 -Create a service for currency conversion -Invoking CurrencyExchange service form CurrencyCalculation service

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/model/CurrencyConversionDto.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.currencyconversionservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversionDto {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private Integer port;
+}
