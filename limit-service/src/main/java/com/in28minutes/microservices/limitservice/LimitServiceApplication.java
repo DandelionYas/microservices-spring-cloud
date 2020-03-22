5c847ed95e3bb823aa8868ea3770b6090459d187
Yaser <yaser.ghaderipoor@gmail.com>
2020-03-22 22:24:16 +0430

    Microservices with spring cloud Step 27 -Connecting currency-exchange-service and limit-service to Eureka Naming Server

--- limit-service/src/main/java/com/in28minutes/microservices/limitservice/LimitServiceApplication.java
+++ limit-service/src/main/java/com/in28minutes/microservices/limitservice/LimitServiceApplication.java
@@ -3,9 +3,11 @@ package com.in28minutes.microservices.limitservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class LimitServiceApplication {
 
     public static void main(String[] args) {
